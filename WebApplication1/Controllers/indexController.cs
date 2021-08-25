3a000184b3cdecf1ef8c5229e186e2d22aff4210
Gitreef Akram <gitreefa12@gmail.com>
2021-08-25 15:12:58 -0700

    Crud operations in Asp.net
    How to insert  data from a form to database & how to display the data in list from the database.

---
+++ WebApplication1/Controllers/indexController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: index
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(tbl_category category)
+
+        {
+
+            WebAppDbContext db = new WebAppDbContext();
+            db.tbl_category.Add(category);
+            db.SaveChanges();
+
+            return View();
+        }
+        public ActionResult list()
+
+        {
+
+            WebAppDbContext db = new WebAppDbContext();
+            var list = db.tbl_category.ToList();
+            
+
+            return View(list);
+        }
+
+    }
+}
